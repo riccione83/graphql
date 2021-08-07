b6d5f40087474e16e72bb7e46e15308f59b828aa
Riccardo Rizzo <riccardo.rizzo@ovoenergy.com>
2021-08-07 22:37:24 +0100

    Hook to fetch data from api

---
+++ web/src/hooks/useGet.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export const useGet = (url: string) => {
+  const [data, setData] = useState({ hits: [] });
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const result = await axios(url);
+
+        setData(result.data);
+      } catch (error) {
+        setIsError(true);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, isLoading, isError };
+};
